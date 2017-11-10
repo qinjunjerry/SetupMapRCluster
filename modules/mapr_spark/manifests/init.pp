b54b3c4e746649e64b5e53147b099763aa99e160
qinjunjerry <qinjunjerry@gmail.com>
2017-11-10 19:42:23 +0100

    configure spark, add mapr_spark_historyserver

--- modules/mapr_spark/manifests/init.pp
+++ modules/mapr_spark/manifests/init.pp
@@ -10,20 +10,55 @@ class mapr_spark (
   require mapr_repo
   require mapr_user
 
+  $spark_home = '/opt/mapr/spark/spark-2.1.0'
+
+  exec { 'mkdir /apps/spark':
+    command   => "/usr/bin/hadoop fs -mkdir /apps/spark",
+    logoutput => on_failure,
+    unless    => "/usr/bin/hadoop fs -ls /apps/spark",
+  }
+  ->
+  exec { 'chmod /apps/spark':
+    command   => "/usr/bin/hadoop fs -chmod 777 /apps/spark",
+    logoutput => on_failure,
+    unless    => "/usr/bin/hadoop fs -ls -d /apps/spark | grep ^drwxrwxrwx",
+  }
+  ->
   package { 'mapr-spark':
     ensure  => present,
     notify  => Class['mapr_config_r'],
   }
+  # Configure Spark JAR Location (Spark 2.0.1 and later)
   ->
-  exec { 'mkdir /apps/spark':
-  	command   => "/usr/bin/hadoop fs -mkdir /apps/spark",
+  exec { "zip $spark_home/jars/*":
+    cwd       => '$spark_home/jars',
+    command   => 'zip $spark_home/spark-jars.zip ./*',
+    creates   => '$spark_home/spark-jars.zip',
     logoutput => on_failure,
-	unless    => "/usr/bin/hadoop fs -ls /apps/spark",
   }
   ->
-  exec { 'chmod /apps/spark':
-  	command   => "/usr/bin/hadoop fs -chmod 777 /apps/spark",
+  exec { 'upload spark-jars.zip':
+    command   => "hadoop fs -put $spark_home/spark-jars.zip /user/mapr/",
     logoutput => on_failure,
-	unless    => "/usr/bin/hadoop fs -ls -d /apps/spark | grep ^drwxrwxrwx",
+    unless    => 'hadoop fs -ls /user/mapr/spark-jars.zip',
   }
+  ->
+  file_line { 'spark.yarn.archive':
+    ensure => present,
+    path   => "$spark_home/conf/spark-defaults.conf",
+    line   => "spark.yarn.archive maprfs:///user/mapr/spark-jars.zip",
+    match  => '^spark.yarn.archive',
+  }
+
+  # Configure Spark with the NodeManager Local Directory Set to MapR-FS:
+  #
+  # sudo -u mapr maprcli volume create -name mapr.$(hostname -f).local.spark \
+  # -path /var/mapr/local/$(hostname -f)/spark -replication 1 -localvolumehost $(hostname -f)
+  #
+  # yarn-site.xml
+  # <property>
+  #   <name>yarn.nodemanager.local-dirs</name>
+  #   <value>/mapr/my.cluster.com/var/mapr/local/${mapr.host}/spark</value>
+  # </property>
+
 }
