ead1d282fe316d6a03f93d6332f876fe6dba401f
qinjunjerry <qinjunjerry@gmail.com>
2017-11-12 12:35:35 +0100

    add cluster_name, bug fix: scripts

--- modules/mapr_core/manifests/cldb_ready.pp
+++ modules/mapr_core/manifests/cldb_ready.pp
@@ -4,18 +4,19 @@
 #
 
 class mapr_core::cldb_ready (
+  $cluster_name = $mapr_config::cluster_name,
 ) {
 
   require mapr_config
 
   exec { 'kinit mapr':
-    command   => "/bin/kinit -kt /opt/mapr/conf/mapr.keytab mapr",
+    command   => "/bin/kinit -kt /opt/mapr/conf/mapr.keytab mapr/$cluster_name",
     logoutput => on_failure,
     require   => File['/opt/mapr/conf/mapr.keytab'],
   }
   ->
   exec { "ensure cldb ready":
-    command   => "/MapRSetup/scrips/ensure_cldb_ready.sh",
+    command   => "/MapRSetup/scripts/ensure_cldb_ready.sh",
     logoutput => on_failure,
   }
 }
