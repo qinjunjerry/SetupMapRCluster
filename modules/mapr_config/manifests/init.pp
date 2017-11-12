fe63df6800a776541d051ce3c0dca38293da0cb8
qinjunjerry <qinjunjerry@gmail.com>
2017-11-12 17:28:33 +0100

    use exec instead of generate a script and run

--- modules/mapr_config/manifests/init.pp
+++ modules/mapr_config/manifests/init.pp
@@ -7,6 +7,7 @@ class mapr_config (
   $cluster_name   = $mapr_config::cluster_name,
   $cldb_node_list = $mapr_config::cldb_node_list,
   $zk_node_list   = $mapr_config::zk_node_list,
+  $historyserver  = $mapr_config::historyserver,
   $secure         = $mapr_config::secure,
   $kerberos       = $mapr_config::kerberos,
   $disk_list      = $mapr_config::disk_list,
@@ -26,20 +27,36 @@ class mapr_config (
     default => ""
   }
 
-  # generate configure_mapr.sh
-  file { '/MapRSetup/configure_mapr.sh':
-    ensure  => present,
-    content => epp('mapr_config/configure_mapr.sh.epp'),
-  } ~>
-  # run configure_mapr.sh
-  exec { 'run configure_mapr.sh':
-    command     => '/bin/sh /MapRSetup/configure_mapr.sh',
+  $disk_file = '/tmp/disks.txt'
+
+  exec { 'run configure.sh':
+    command     => join(['/opt/mapr/server/configure.sh',
+                         ' ', '-N', ' ', $cluster_name,
+                         ' ', '-Z', ' ', $zk_node_list,
+                         ' ', '-C', ' ', $cldb_node_list,
+                         ' ', '-HS', ' ', $historyserver,
+                         ' ', $secure_opt,
+                         ' ', $kerberos_opt,
+                         ' ', '-nocerts',
+                         ' ', '-no-autostart',
+                         ' ', '-f',
+                       ]),
+    logoutput   => on_failure,
     refreshonly => true,
-  } ->
+  }
+  ~>
+  exec { 'create disks.txt':
+    command   => "echo $disk_list > $disk_file && /opt/mapr/server/disksetup -F $disk_file",
+    path      => '/usr/bin:/usr/sbin:/bin',
+    logoutput => on_failure,
+    creates   => '/opt/mapr/conf/disktab',
+  }
+  ~>
   service { 'mapr-zookeeper':
     enable      => true,
     ensure      => running,
-  } ->
+  }
+  ~>
   service { 'mapr-warden':
     enable      => true,
     ensure      => running,
