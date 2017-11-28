f223d55ab5e345a4f7898c15d25450021a35dbf6
qinjunjerry <qinjunjerry@gmail.com>
2017-11-28 17:34:14 +0100

    configure resourcemanager_host and webhdfs_url in hue

--- modules/profile/manifests/mapr/ecosystem/hue.pp
+++ modules/profile/manifests/mapr/ecosystem/hue.pp
@@ -13,4 +13,26 @@ class profile::mapr::ecosystem::hue (
     notify  => Class['profile::mapr::configure_r']
   }
 
+  $version = fact('mapr-hue:version')
+  $cfgfile = "/opt/mapr/hue/hue-$version/desktop/conf/hue.ini"
+
+  file_line {'resourcemanager_host in hue.ini':
+  	ensure   => 'present',
+  	line     => '      resourcemanager_host=maprfs:///',
+  	path     => $cfgfile,
+  	after    => '## resourcemanager_host=localhost',
+  	match    => '^\s*resourcemanager_host\=',
+  	require  => Package['mapr-hue'],
+  }
+
+  file_line {'webhdfs_url in hue.ini':
+  	ensure   => 'present',
+  	line     => '      webhdfs_url=http://node69:14000/webhdfs/v1',
+  	path     => $cfgfile,
+  	after    => '# Default port is 14000 for HttpFs.',
+  	match    => '^\s*webhdfs_url\=',
+  	require  => Package['mapr-hue'],
+  }
+
+
 }
