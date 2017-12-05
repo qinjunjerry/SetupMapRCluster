8a59ebccefb4ecf2eed74c3dbcc0913fdb4289c8
qinjunjerry <qinjunjerry@gmail.com>
2017-12-05 22:17:26 +0100

    support create kerberos principal with fqdn

--- utils/create_principal_keytab.sh
+++ utils/create_principal_keytab.sh
@@ -1,35 +1,35 @@
 #!/bin/sh
 
 if [ $# -lt 3 ]; then
-    echo Usage: $0 cluster user node [node ...]
+    echo Usage: $0 cluster domain node [node ...]
     exit 1
 fi
 
 create_principal() {
-	principal=$1
-	passwd=$2
+    principal=$1
+    passwd=$2
 
-	if [ "x$passwd" != "x" ]; then
-		passwd="-pw $passwd"
-	fi
+    if [ "x$passwd" != "x" ]; then
+        passwd="-pw $passwd"
+    fi
 
-	if kadmin.local -q 'listprincs' | grep -q ${principal}@; then
-		echo principal $principal aleady exits
-	else
-		kadmin.local -q "addprinc -randkey $passwd $principal"
-	fi
+    if kadmin.local -q 'listprincs' | grep -q ${principal}@; then
+        echo principal $principal aleady exits
+    else
+        kadmin.local -q "addprinc -randkey $passwd $principal"
+    fi
 }
 
 CLUSTER_NAME=$1
-USER_NAME=$2
+DOMAIN_NAME=$2
 
 create_principal mapr/$CLUSTER_NAME mapr
-create_principal $USER_NAME mapr
 
 shift
 shift
 
 for node in $*; do
+    node=$node.$DOMAIN_NAME
     create_principal mapr/$node
     create_principal HTTP/$node
 
---
+++ utils/delete_principal_keytab.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+if [ $# -lt 3 ]; then
+    echo Usage: $0 cluster domain node [node ...]
+    exit 1
+fi
+
+delete_principal() {
+    principal=$1
+
+    if kadmin.local -q 'listprincs' | grep -q ${principal}@; then
+        kadmin.local -q "delprinc -force $principal"
+    else
+        echo principal $principal not found
+    fi
+}
+
+CLUSTER_NAME=$1
+DOMAIN_NAME=$2
+
+delete_principal mapr/$CLUSTER_NAME
+
+shift
+shift
+
+for node in $*; do
+    node=$node.$DOMAIN_NAME
+
+    delete_principal mapr/$node
+    delete_principal HTTP/$node
+
+    rm -fr $node
+done
+
