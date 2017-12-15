ccce2312555a964dd025fb98c5eede49468ec5df
qinjunjerry <qinjunjerry@gmail.com>
2017-12-15 14:52:34 +0100

    remove cldb,zk,rm,webserver from role::cluster_node

--- modules/role/manifests/cluster_node.pp
+++ modules/role/manifests/cluster_node.pp
@@ -3,16 +3,11 @@ class role::cluster_node {
   include role::mapr_common
 
   include profile::mapr::core::core
-  include profile::mapr::core::zookeeper
-  include profile::mapr::core::cldb
   include profile::mapr::core::fileserver
   include profile::mapr::core::nfs
 
-  include profile::mapr::core::resourcemanager
   include profile::mapr::core::nodemanager
   include profile::mapr::core::rm_nm_common
-
-  include profile::mapr::core::webserver
 
   include profile::mapr::sasl
   include profile::mapr::kerberos
