2bfcac3ed342053a683edaabe1465404c9d47b19
qinjunjerry <qinjunjerry@gmail.com>
2018-01-23 15:41:09 +0100

    generate node specific ssl_keystore/truststore

--- modules/profile/manifests/mapr/sasl.pp
+++ modules/profile/manifests/mapr/sasl.pp
@@ -11,16 +11,24 @@ class profile::mapr::sasl (
 
     include profile::mapr::cluster
 
+    $hostname = fact('networking.fqdn')
+
     file {
       default:
         ensure => file,
         owner  => 'mapr',
         group  => 'mapr',
         mode   => '0600';
-      '/opt/mapr/conf/cldb.key'        : source  => "$::base_dir/inputfiles/$profile::mapr::cluster::cluster_name/cldb.key";
-      '/opt/mapr/conf/maprserverticket': source  => "$::base_dir/inputfiles/$profile::mapr::cluster::cluster_name/maprserverticket";
-      '/opt/mapr/conf/ssl_keystore'    : source  => "$::base_dir/inputfiles/$profile::mapr::cluster::cluster_name/ssl_keystore",  mode => '0400';
-      '/opt/mapr/conf/ssl_truststore'  : source  => "$::base_dir/inputfiles/$profile::mapr::cluster::cluster_name/ssl_truststore",mode => '0444';
+      '/opt/mapr/conf/cldb.key'        : source => "$::base_dir/inputfiles/$profile::mapr::cluster::cluster_name/cldb.key";
+      '/opt/mapr/conf/maprserverticket': source => "$::base_dir/inputfiles/$profile::mapr::cluster::cluster_name/maprserverticket";
+      '/opt/mapr/conf/ssl_keystore'    : mode   => '0400', source => [
+            "$::base_dir/inputfiles/$profile::mapr::cluster::cluster_name/$hostname/ssl_keystore",
+            "$::base_dir/inputfiles/$profile::mapr::cluster::cluster_name/ssl_keystore",
+      ];
+      '/opt/mapr/conf/ssl_truststore'  : mode   => '0444', source => [
+            "$::base_dir/inputfiles/$profile::mapr::cluster::cluster_name/$hostname/ssl_truststore",
+            "$::base_dir/inputfiles/$profile::mapr::cluster::cluster_name/ssl_truststore",
+      ];
     }
   }
 
--- utils/create_cldb_keyfile.sh
+++ utils/create_cldb_keyfile.sh
@@ -1,37 +1,21 @@
 #!/bin/sh
 
 if [ $# -lt 2 ]; then
-    echo Usage: $0 cluster dnsdomain
+    echo Usage: $0 cluster dnsdomain node [node ...]
     exit 1
 fi
 
 CLUSTER_NAME=$1
-CERT_NAME=*.$2
+DNS_DOMAIN=$2
+shift
+shift
 
 mkdir -p $CLUSTER_NAME/
 
-if [ -e cldb.key -o -e maprserverticket -o -e ssl_keystore -o -e ssl_truststore ]; then
-	echo At least one of cldb.key, maprserverticket, ssl_keystore, and ssl_truststore exists, exiting now.
+if [ -e $CLUSTER_NAME/cldb.key -o -e $CLUSTER_NAME/maprserverticket ]; then
+	echo At least one of $CLUSTER_NAME/cldb.key, $CLUSTER_NAME/maprserverticket exists, exiting now.
 	exit 1
 fi
-
-if [ `pwd` = '/opt/mapr/conf' ]; then
-	echo This script must not run from /opt/mapr/conf, to avoid overriding existing cluster key files
-	exit 1
-fi
-
-# cldb.key
-/opt/mapr/bin/maprcli security genkey -keyfile $CLUSTER_NAME/cldb.key
-
-# maprserverticket
-/opt/mapr/bin/maprcli security genticket -inkeyfile $CLUSTER_NAME/cldb.key -ticketfile $CLUSTER_NAME/maprserverticket -cluster $CLUSTER_NAME -maprusername mapr -mapruid 5000 -maprgid 5000
-
-### generate ssl key/truststore
-sslKeyStore=$CLUSTER_NAME/ssl_keystore
-sslTrustStore=$CLUSTER_NAME/ssl_truststore
-
-storePass=mapr123
-storeFormat=JKS
 
 if [ "$JAVA_HOME"x = "x" ]; then
   KEYTOOL=`which keytool`
@@ -46,32 +30,55 @@ if [ ! -e "${KEYTOOL:-}" ]; then
     exit 1
 fi
 
+# cldb.key
+/opt/mapr/bin/maprcli security genkey -keyfile $CLUSTER_NAME/cldb.key
 
-# ssl_keystore
-# create self signed certificate with private key
-echo "Creating 10 year self signed certificate with subjectDN=CN=$CERT_NAME"
-$KEYTOOL -genkeypair -sigalg SHA512withRSA -keyalg RSA -alias $CLUSTER_NAME -dname CN=$CERT_NAME -validity 3650 \
-       -storepass $storePass -keypass $storePass \
-       -keystore $sslKeyStore -storetype $storeFormat
-if [ $? -ne 0 ]; then
-    echo "Keytool command to generate key store failed"
-    exit 1
-fi
+# maprserverticket
+/opt/mapr/bin/maprcli security genticket -inkeyfile $CLUSTER_NAME/cldb.key -ticketfile $CLUSTER_NAME/maprserverticket -cluster $CLUSTER_NAME -maprusername mapr -mapruid 5000 -maprgid 5000
 
-# ssl_truststore
-# extract self signed certificate into trust store
-tmpfile=$sslTrustStore.tmp
-rm -f $tmpfile
-$KEYTOOL -exportcert -keystore $sslKeyStore -file $tmpfile \
-       -alias $CLUSTER_NAME -storepass $storePass -storetype $storeFormat
-if [ $? -ne 0 ]; then
-    echo "Keytool command to extract certificate from key store failed"
-    exit 1
-fi
-$KEYTOOL -importcert -keystore $sslTrustStore -file $tmpfile \
-        -alias $CLUSTER_NAME -storepass $storePass -noprompt
-if [ $? -ne 0 ]; then
-    echo "Keytool command to create trust store failed"
-    exit 1
-fi
-rm -f $tmpfile
+### generate ssl key/truststore
+storePass=mapr123
+storeFormat=JKS
+
+for node in $*; do
+
+  CERT_NAME=$node.$DNS_DOMAIN
+
+  mkdir -p $CLUSTER_NAME/$CERT_NAME
+
+  sslKeyStore=$CLUSTER_NAME/$CERT_NAME/ssl_keystore
+  sslTrustStore=$CLUSTER_NAME/$CERT_NAME/ssl_truststore
+
+  IP_ADDR=`hostname -i`
+
+  # ssl_keystore
+  # create self signed certificate with private key
+  echo "Creating 10 year self signed certificate with subjectDN=CN=$CERT_NAME"
+  $KEYTOOL -genkeypair -sigalg SHA512withRSA -keyalg RSA -alias $CLUSTER_NAME -dname CN=$CERT_NAME -validity 3650 \
+         -ext "san=dns:$node,ip:$IP_ADDR" \
+         -storepass $storePass -keypass $storePass \
+         -keystore $sslKeyStore -storetype $storeFormat
+  if [ $? -ne 0 ]; then
+      echo "Keytool command to generate key store failed"
+      exit 1
+  fi
+
+  # ssl_truststore
+  # extract self signed certificate into trust store
+  tmpfile=$sslTrustStore.tmp
+  rm -f $tmpfile
+  $KEYTOOL -exportcert -keystore $sslKeyStore -file $tmpfile \
+         -alias $CLUSTER_NAME -storepass $storePass -storetype $storeFormat
+  if [ $? -ne 0 ]; then
+      echo "Keytool command to extract certificate from key store failed"
+      exit 1
+  fi
+  $KEYTOOL -importcert -keystore $sslTrustStore -file $tmpfile \
+          -alias $CLUSTER_NAME -storepass $storePass -noprompt
+  if [ $? -ne 0 ]; then
+      echo "Keytool command to create trust store failed"
+      exit 1
+  fi
+  rm -f $tmpfile
+
+done
