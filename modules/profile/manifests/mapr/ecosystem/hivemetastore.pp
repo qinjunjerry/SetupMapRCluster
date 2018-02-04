b1f20bdf36d7de7b98c69047a6fbf5086a4920b4
qinjunjerry <qinjunjerry@gmail.com>
2018-02-04 23:00:38 +0100

    configure hive metastore to use a remote mysql database

--- modules/profile/manifests/mapr/ecosystem/hivemetastore.pp
+++ modules/profile/manifests/mapr/ecosystem/hivemetastore.pp
@@ -6,11 +6,17 @@
 class profile::mapr::ecosystem::hivemetastore (
 ) {
 
+  include profile::mapr::cluster
   require profile::mapr::configure
 
 
   $version = fact('mapr-hive:version')
   $cfgfile = "/opt/mapr/hive/hive-$version/conf/hive-site.xml"
+
+  $mysql_node     = $profile::mapr::cluster::mysql_node
+  $hive_meta_node = $profile::mapr::cluster::hive_meta_node
+  $metastore_db   = regsubst($profile::mapr::cluster::cluster_name, '\.', '_', 'G')
+
 
   package { 'mapr-hivemetastore':
     ensure  => present,
@@ -25,6 +31,37 @@ class profile::mapr::ecosystem::hivemetastore (
       notify => Class['profile::mapr::warden_restart'];
 
     "hive.metastore.execute.setugi"    : value =>"true";
+  }
+
+  # Configure MySQL for hive metastore
+  ->
+  package { 'mysql-connector-java':
+    ensure  => present,
+    require => Package['mapr-hivemetastore'];
+  }
+  ->
+  exec { 'link mysql-connector-jar.jar':
+    command   => "/usr/bin/ln -s /usr/share/java/mysql-connector-java.jar /opt/mapr/hive/hive-$version/lib/mysql-connector-java.jar",
+    logoutput => on_failure,
+    creates   => "/opt/mapr/hive/hive-$version/lib/mysql-connector-java.jar",
+  }
+  ->
+  profile::hadoop::xmlconf_property {
+    default:
+      file   => $cfgfile,
+      notify => Class['profile::mapr::warden_restart'];
+
+    "javax.jdo.option.ConnectionURL"        : value => "jdbc:mysql://$mysql_node/metastore_$metastore_db";
+    "javax.jdo.option.ConnectionDriverName" : value => 'com.mysql.jdbc.Driver';
+    "javax.jdo.option.ConnectionUserName"   : value => 'hive';
+    "javax.jdo.option.ConnectionPassword"   : value => 'password';
+    "hive.metastore.uris"                   : value => "thrift://$hive_meta_node:9083";
+  }
+  ->
+  exec { 'initSchema':
+    command   => "/opt/mapr/hive/hive-$version/bin/schematool -dbType mysql -initSchema",
+    logoutput => on_failure,
+    unless    => "/opt/mapr/hive/hive-2.1/bin/schematool -dbType mysql -info",
   }
 
 }
--- modules/profile/manifests/mapr/ecosystem/hiveserver2.pp
+++ modules/profile/manifests/mapr/ecosystem/hiveserver2.pp
@@ -6,10 +6,12 @@
 class profile::mapr::ecosystem::hiveserver2 (
 ) {
 
+  include profile::mapr::cluster
   require profile::mapr::configure
 
   $version = fact('mapr-hive:version')
   $cfgfile = "/opt/mapr/hive/hive-$version/conf/hive-site.xml"
+  $hive_meta_node = $profile::mapr::cluster::hive_meta_node
 
   package { 'mapr-hiveserver2':
     ensure  => present,
@@ -26,4 +28,15 @@ class profile::mapr::ecosystem::hiveserver2 (
     "hive.server2.enable.doAs"                   : value =>"true";
     "hive.metastore.execute.setugi"              : value =>"true";
   }
+
+  # Needed when using mysql for hive metastore
+  ->
+  profile::hadoop::xmlconf_property {
+    default:
+      file   => $cfgfile,
+      notify => Class['profile::mapr::warden_restart'];
+
+    "hive.metastore.uris" : value => "thrift://$hive_meta_node:9083";
+  }
+
 }
