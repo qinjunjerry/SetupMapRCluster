41ce8524dde64832863b961b58e7bf9a96c94ccb
qinjunjerry <qinjunjerry@gmail.com>
2018-06-08 16:31:53 +0200

    add META_NODE_SHORT also in mysql

--- utils/delete_hive_meta.sh
+++ utils/delete_hive_meta.sh
@@ -7,10 +7,14 @@ fi
 
 CLUSTER=$1
 META_NODE=$2
+
 META_DB=`echo $CLUSTER | tr . _`
+META_NODE_SHORT=`echo $2 | cut -d. -f 1`
 
 
 
 echo "\
 DROP DATABASE metastore_$META_DB; \
-DROP USER hive@$META_NODE; " | mysql -u root --password=
+DROP USER hive@$META_NODE; \
+DROP USER hive@$META_NODE_SHORT; \
+" | mysql -u root --password=
