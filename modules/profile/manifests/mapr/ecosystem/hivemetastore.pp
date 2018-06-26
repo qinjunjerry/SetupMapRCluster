d638d5f308449de9108ab5df8a8d5c72480dd348
qinjunjerry <qinjunjerry@gmail.com>
2018-06-26 09:03:49 +0200

    Add kerberos related config only when kerberos is enabled

--- modules/profile/manifests/mapr/ecosystem/hivemetastore.pp
+++ modules/profile/manifests/mapr/ecosystem/hivemetastore.pp
@@ -66,38 +66,42 @@ class profile::mapr::ecosystem::hivemetastore (
   }
 
   # Configure Hive metastore to use kerberos
-  #TODO: check security flag
-  ->
-  profile::hadoop::xmlconf_property {
-    default:
-      file   => $cfgfile,
-      notify => Class['profile::mapr::warden_restart'];
+  
+  if $profile::mapr::cluster::kerberos == true {
+ 
+    profile::hadoop::xmlconf_property {
+      default:
+        file    => $cfgfile,
+        require => Package['mapr-hivemetastore'],
+        notify  => Class['profile::mapr::warden_restart'];
 
-    "hive.metastore.kerberos.keytab.file"   : value => '/opt/mapr/conf/mapr.keytab';
-    "hive.metastore.kerberos.principal"     : value => "mapr/$hostname@$::profile::kerberos::default_realm";
-  }
-  ->
-  file_line {'MAPR_HIVE_LOGIN_OPTS in /opt/mapr/conf/env.sh':
-    ensure   => 'present',
-    path     => '/opt/mapr/conf/env.sh',
-    line     => '    MAPR_HIVE_LOGIN_OPTS="-Dhadoop.login=hybrid"',
-    match    => '^\s+MAPR_HIVE_LOGIN_OPTS\="-Dhadoop.login=maprsasl"',
-    notify   => Class['profile::mapr::warden_restart'],
-  }
-  ->
-  file_line {'MAPR_HIVE_SERVER_LOGIN_OPTS in /opt/mapr/conf/env.sh':
-    ensure   => 'present',
-    path     => '/opt/mapr/conf/env.sh',
-    line     => '    MAPR_HIVE_SERVER_LOGIN_OPTS="-Dhadoop.login=hybrid"',
-    match    => '^\s+MAPR_HIVE_SERVER_LOGIN_OPTS\="-Dhadoop.login=maprsasl_keytab"',
-    notify   => Class['profile::mapr::warden_restart'],
-  }
-  ->
-  file_line { "HADOOP_OPTS in /opt/mapr/hive/hive-$version/bin/ext/beeline.sh":
-    ensure   => 'present',
-    path     => "/opt/mapr/hive/hive-$version/bin/ext/beeline.sh",
-    line     => '  export HADOOP_OPTS="$HADOOP_OPTS ${KERBEROS_LOGIN_OPTS}"',
-    match    => '^\s+export HADOOP_OPTS\=',
+      "hive.metastore.kerberos.keytab.file"   : value => '/opt/mapr/conf/mapr.keytab';
+      "hive.metastore.kerberos.principal"     : value => "mapr/$hostname@$::profile::kerberos::default_realm";
+    }
+    ->
+    file_line {'MAPR_HIVE_LOGIN_OPTS in /opt/mapr/conf/env.sh':
+      ensure   => 'present',
+      path     => '/opt/mapr/conf/env.sh',
+      line     => '    MAPR_HIVE_LOGIN_OPTS="-Dhadoop.login=hybrid"',
+      match    => '^\s+MAPR_HIVE_LOGIN_OPTS\="-Dhadoop.login=maprsasl"',
+      notify   => Class['profile::mapr::warden_restart'],
+    }
+    ->
+    file_line {'MAPR_HIVE_SERVER_LOGIN_OPTS in /opt/mapr/conf/env.sh':
+      ensure   => 'present',
+      path     => '/opt/mapr/conf/env.sh',
+      line     => '    MAPR_HIVE_SERVER_LOGIN_OPTS="-Dhadoop.login=hybrid"',
+      match    => '^\s+MAPR_HIVE_SERVER_LOGIN_OPTS\="-Dhadoop.login=maprsasl_keytab"',
+      notify   => Class['profile::mapr::warden_restart'],
+    }
+    ->
+    file_line { "HADOOP_OPTS in /opt/mapr/hive/hive-$version/bin/ext/beeline.sh":
+      ensure   => 'present',
+      path     => "/opt/mapr/hive/hive-$version/bin/ext/beeline.sh",
+      line     => '  export HADOOP_OPTS="$HADOOP_OPTS ${KERBEROS_LOGIN_OPTS}"',
+      match    => '^\s+export HADOOP_OPTS\=',
+    }
+
   }
 
 }
--- modules/profile/manifests/mapr/ecosystem/hiveserver2.pp
+++ modules/profile/manifests/mapr/ecosystem/hiveserver2.pp
@@ -42,39 +42,43 @@ class profile::mapr::ecosystem::hiveserver2 (
   }
 
   # Configure Hive Server2 to use kerberos
-  ->
-  profile::hadoop::xmlconf_property {
-    default:
-      file   => $cfgfile,
-      notify => Class['profile::mapr::warden_restart'];
+  if $profile::mapr::cluster::kerberos == true {
+  
+    profile::hadoop::xmlconf_property {
+      default:
+        file    => $cfgfile,
+        require => Package['mapr-hiveserver2'],
+        notify  => Class['profile::mapr::warden_restart'];
 
-    "hive.server2.authentication"                   : value => 'KERBEROS';
-    "hive.server2.authentication.kerberos.keytab"   : value => '/opt/mapr/conf/mapr.keytab';
-    "hive.server2.authentication.kerberos.principal": value => "mapr/$hostname@$::profile::kerberos::default_realm";
-    "hive.metastore.kerberos.principal@hs2"         : value => "mapr/$hive_meta_node@$::profile::kerberos::default_realm";
-  }
-  ->
-  file_line { 'MAPR_HIVE_LOGIN_OPTS in /opt/mapr/conf/env.sh@hs2':
-    ensure   => 'present',
-    path     => '/opt/mapr/conf/env.sh',
-    line     => '    MAPR_HIVE_LOGIN_OPTS="-Dhadoop.login=hybrid"',
-    match    => '^\s+MAPR_HIVE_LOGIN_OPTS\="-Dhadoop.login=maprsasl"',
-    notify   => Class['profile::mapr::warden_restart'],
-  }
-  ->
-  file_line { 'MAPR_HIVE_SERVER_LOGIN_OPTS in /opt/mapr/conf/env.sh@hs2':
-    ensure   => 'present',
-    path     => '/opt/mapr/conf/env.sh',
-    line     => '    MAPR_HIVE_SERVER_LOGIN_OPTS="-Dhadoop.login=hybrid"',
-    match    => '^\s+MAPR_HIVE_SERVER_LOGIN_OPTS\="-Dhadoop.login=maprsasl_keytab"',
-    notify   => Class['profile::mapr::warden_restart'],
-  }
-  ->
-  file_line { "HADOOP_OPTS in /opt/mapr/hive/hive-$version/bin/ext/beeline.sh@hs2":
-    ensure   => 'present',
-    path     => "/opt/mapr/hive/hive-$version/bin/ext/beeline.sh",
-    line     => '  export HADOOP_OPTS="$HADOOP_OPTS ${KERBEROS_LOGIN_OPTS}"',
-    match    => '^\s+export HADOOP_OPTS\=',
+      "hive.server2.authentication"                   : value => 'KERBEROS';
+      "hive.server2.authentication.kerberos.keytab"   : value => '/opt/mapr/conf/mapr.keytab';
+      "hive.server2.authentication.kerberos.principal": value => "mapr/$hostname@$::profile::kerberos::default_realm";
+      "hive.metastore.kerberos.principal@hs2"         : value => "mapr/$hive_meta_node@$::profile::kerberos::default_realm";
+    }
+    ->
+    file_line { 'MAPR_HIVE_LOGIN_OPTS in /opt/mapr/conf/env.sh@hs2':
+      ensure   => 'present',
+      path     => '/opt/mapr/conf/env.sh',
+      line     => '    MAPR_HIVE_LOGIN_OPTS="-Dhadoop.login=hybrid"',
+      match    => '^\s+MAPR_HIVE_LOGIN_OPTS\="-Dhadoop.login=maprsasl"',
+      notify   => Class['profile::mapr::warden_restart'],
+    }
+    ->
+    file_line { 'MAPR_HIVE_SERVER_LOGIN_OPTS in /opt/mapr/conf/env.sh@hs2':
+      ensure   => 'present',
+      path     => '/opt/mapr/conf/env.sh',
+      line     => '    MAPR_HIVE_SERVER_LOGIN_OPTS="-Dhadoop.login=hybrid"',
+      match    => '^\s+MAPR_HIVE_SERVER_LOGIN_OPTS\="-Dhadoop.login=maprsasl_keytab"',
+      notify   => Class['profile::mapr::warden_restart'],
+    }
+    ->
+    file_line { "HADOOP_OPTS in /opt/mapr/hive/hive-$version/bin/ext/beeline.sh@hs2":
+      ensure   => 'present',
+      path     => "/opt/mapr/hive/hive-$version/bin/ext/beeline.sh",
+      line     => '  export HADOOP_OPTS="$HADOOP_OPTS ${KERBEROS_LOGIN_OPTS}"',
+      match    => '^\s+export HADOOP_OPTS\=',
+    }
+
   }
 
 }
