6a6228d5586ec79f092280e2e1cdd14ac26982c6
qinjunjerry <qinjunjerry@gmail.com>
2018-06-28 09:04:44 +0200

    add opetsdb purge data crontab, needed in MapR 6 where the crontab is commented out initially

--- modules/profile/manifests/mapr/ecosystem/opentsdb.pp
+++ modules/profile/manifests/mapr/ecosystem/opentsdb.pp
@@ -8,9 +8,19 @@ class profile::mapr::ecosystem::opentsdb (
 
   require profile::mapr::configure_r
 
+  $version = fact('mapr-opentsdb:version')
+
   package { 'mapr-opentsdb':
     ensure  => present,
     notify  => Class['profile::mapr::configure_r_ot'],
+  }
+
+  ->
+  cron { 'opentsdb purge data':
+    command => "/opt/mapr/opentsdb/opentsdb-$version/bin/tsdb_cluster_mgmt.sh -purgeData >> /opt/mapr/opentsdb/opentsdb-$version/var/log/opentsdb/ot_purgeData.log 2>&1",
+    user    => 'mapr',
+    hour    => 1,
+    minute  => 15,
   }
 
 }
