a5a92e2eb684703fc91143839986a8670f9a8c9f
qinjunjerry <qinjunjerry@gmail.com>
2018-10-01 20:48:12 +0200

    Bugfix: use

--- utils/ensure_nodemanager_ready.sh
+++ utils/ensure_nodemanager_ready.sh
@@ -8,7 +8,7 @@ while ! jps | grep NodeManager 2>&1 >>$LOGFILE; do
 	echo `date`: sleep 10 >> $LOGFILE
 	sleep 10
 done
-echo `date`: "NodeManager is up, check /var/mapr/local/$(hostname -f)" >> LOGFILE
+echo `date`: "NodeManager is up, check /var/mapr/local/$(hostname -f)" >> $LOGFILE
 while ! hadoop fs -test -d /var/mapr/local/$(hostname -f) 2>&1 >>$LOGFILE; do
 	echo `date`: sleep 10 >> $LOGFILE
 	sleep 10
