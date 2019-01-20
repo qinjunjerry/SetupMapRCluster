69bde1f017a8de1d951bcd1031ec98460120c663
qinjunjerry <qinjunjerry@gmail.com>
2019-01-20 21:12:02 +0100

    use role::cluster_node_core in role::cluster_node

--- modules/role/manifests/cluster_node.pp
+++ modules/role/manifests/cluster_node.pp
@@ -1,18 +1,6 @@
 class role::cluster_node {
 
-  include role::mapr_common
-
-  include profile::mapr::core::core
-  include profile::mapr::core::fileserver
-  include profile::mapr::core::nfs
-
-  include profile::mapr::core::nodemanager
-  include profile::mapr::core::rm_nm_common
-  # Configure NM local dir to the local volume on MapR FS
-  include profile::mapr::core::nm_local_dirs
-
-  include profile::mapr::sasl
-  include profile::mapr::kerberos
+  include role::cluster_node_core
 
   # monitoring
   include profile::mapr::ecosystem::collectd
@@ -24,9 +12,5 @@ class role::cluster_node {
   include profile::mapr::ecosystem::spark_conf
 
   include profile::mapr::ecosystem::drill
-
-  include profile::mapr::warden_restart
-
-  include profile::mapr::configure
 
 }
