a024ec6556afc1a7f37e2b2ad7f946690daf2f63
qinjunjerry <qinjunjerry@gmail.com>
2019-04-15 11:33:49 +0200

    Support Tez-UI cross-origin support (CORS) for web services at RM side

--- modules/profile/manifests/mapr/core/resourcemanager.pp
+++ modules/profile/manifests/mapr/core/resourcemanager.pp
@@ -15,4 +15,23 @@ class profile::mapr::core::resourcemanager (
     notify  => Class['profile::mapr::configure'],
     before  => Class['profile::mapr::core::rm_nm_common'],
   }
+  ->
+  # Needed by Tez-UI cross-origin support (CORS) for web services
+  profile::hadoop::xmlconf_property {
+    default:
+      file    => '/opt/mapr/hadoop/hadoop-2.7.0/etc/hadoop/core-site.xml',
+      notify  => Class['profile::mapr::warden_restart'];
+      
+    'org.apache.hadoop.security.HttpCrossOriginFilterInitializer': value => 'hadoop.http.filter.initializers';
+  
+  }
+  ->
+  profile::hadoop::xmlconf_property {
+    default:
+      file    => '/opt/mapr/hadoop/hadoop-2.7.0/etc/hadoop/yarn-site.xml',
+      notify  => Class['profile::mapr::warden_restart'];
+
+    'yarn.resourcemanager.webapp.cross-origin.enabled': value => 'true';
+  }  
+
 }
